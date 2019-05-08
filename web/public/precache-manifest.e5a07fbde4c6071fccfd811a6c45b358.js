283848f05a50ad2f7977e5146c53d90b7d960841
zbw0046 <zbw0046@gmail.com>
2019-05-09 00:49:32 +0800

    append new files

---
+++ web/public/precache-manifest.e5a07fbde4c6071fccfd811a6c45b358.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dacb760445be2ba4346257b427158e99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d38ebad62fc1872de5f",
+    "url": "/static/css/main.33750180.chunk.css"
+  },
+  {
+    "revision": "3b20eb9caefa1bec7f0c",
+    "url": "/static/js/2.aad7ca45.chunk.js"
+  },
+  {
+    "revision": "1d38ebad62fc1872de5f",
+    "url": "/static/js/main.0a8efd8e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
